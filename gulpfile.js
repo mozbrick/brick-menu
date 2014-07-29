1405819ef0e85da6dd958bce39fe4b376644caee
dotch <cweiss@mozilla.com>
2014-07-28 17:35:06 -0700

    updated theme handling; removed functionality which kept track of selected item

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,15 @@ var ghpages = require('gulp-gh-pages');
 var gulp = require('gulp');
 var helptext = require('gulp-helptext');
 var jshint = require('gulp-jshint');
+var rm = require('gulp-rm');
 var stylus = require('gulp-stylus');
+var vulcanize = require('gulp-vulcanize');
 
 var paths = {
   'main': 'src/brick-menu.html',
   'scripts': 'src/*.js',
   'stylesheets': 'src/*.styl',
+  'themes': 'src/themes/**/*.styl',
   'src': 'src/*',
   'index': 'index.html',
   'bowerComponents': 'bower_components/**/*'
@@ -26,15 +29,40 @@ gulp.task('lint', function() {
 });
 
 gulp.task('styles', function() {
-  gulp.src(paths.stylesheets)
+  return gulp.src(paths.stylesheets)
     .pipe(stylus())
     .pipe(concat('brick-menu.css'))
     .pipe(gulp.dest('src'));
 });
 
-// build scripts and styles
-gulp.task('build', ['lint','styles']);
+gulp.task('themes', function() {
+  return gulp.src(paths.themes)
+    .pipe(stylus())
+    .pipe(gulp.dest('src/themes/'));
+});
 
+gulp.task('clean', ['vulcanize'], function() {
+  gulp.src(['src/*.css', 'src/themes/**/*.css'])
+    .pipe(rm());
+});
+
+gulp.task('vulcanize', ['styles','themes'], function() {
+  return gulp.src('src/brick-menu.html')
+    .pipe(vulcanize({
+      excludes: {
+        imports: ['bower_components'],
+        scripts: ['bower_components'],
+        styles: ['bower_components']
+      },
+      dest: 'dist',
+      csp: true,
+      inline: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+// build scripts and styles
+gulp.task('build', ['lint','styles','themes','vulcanize', 'clean']);
 
 gulp.task('connect', function() {
   connect.server({
@@ -42,10 +70,10 @@ gulp.task('connect', function() {
   });
 });
 
-
 gulp.task('watch', function () {
   gulp.watch(paths.scripts, ['lint']);
-  gulp.watch(paths.stylesheets, ['styles']);
+  gulp.watch(paths.stylesheets, ['build']);
+  gulp.watch(paths.themes, ['build']);
 });
 
 // do a build, start a server, watch for changes
@@ -61,7 +89,9 @@ gulp.task('bump', function(){
 gulp.task('help', helptext({
   'default': 'Shows the help message',
   'help': 'This help message',
-  'styles': 'Compiles stylus',
+  'styles': 'Compiles main stylus',
+  'themes': 'Compiles themes stylus',
+  'vulcanize': 'Vulcanizes to component html file',
   'lint': 'Runs JSHint on your code',
   'server': 'Starts the development server',
   'bump': 'Bumps up the Version',
